285fe40977b41c933332fb2055c18f171a127586
Cesar- <legende.e@hotmail.com>
2025-11-24 20:10:10 -0500

    feat: add bonus information component with responsive design, translation support, and external link functionality

--- src/app/projects/presentation/views/bonus-information/bonus-information.ts
+++ src/app/projects/presentation/views/bonus-information/bonus-information.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-bonus-information',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './bonus-information.html',
   styleUrl: './bonus-information.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BonusInformation {
+  /**
+   * External link for more information
+   * This can be updated with the actual URL
+   */
+  readonly moreInfoUrl = 'https://www.mivivienda.com.pe/portalweb/usuario-busca-viviendas/pagina.aspx?idpage=30';
 
+  /**
+   * Open external link in new tab
+   */
+  onMoreInfo(): void {
+    window.open(this.moreInfoUrl, '_blank', 'noopener,noreferrer');
+  }
 }
